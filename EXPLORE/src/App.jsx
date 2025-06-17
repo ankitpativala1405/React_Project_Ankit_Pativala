a80188899e1a3a7bed7646150b39d1022efce173
vaghelaharsh1015 <harshvaghela4277@gmail.com>
2025-06-17 15:53:54 +0530

    update || 17-06

--- EXPLORE/src/App.jsx
+++ EXPLORE/src/App.jsx
@@ -8,13 +8,13 @@ function App() {
   return (
     <>
     {/* <h1 className="bg-black text-2rem font-black text-white h-[100px] "><p className="pt-10">Chakra UI</p></h1> */}
-     <h1 className='text-[4rem] bg-[#E2DDDD] h-[80px] font-extrabold'>Chakra UI</h1>
+     <h1 className='text-2xl bg-[#E2DDDD] h-[80px] font-black'>Chakra UI</h1>
       <Provider>
         <Chakra />
       </Provider>
       <h1 className='text-[4rem] bg-[#E2DDDD] h-[80px] font-extrabold'>Flowbite</h1>
       <FlowbiteComponent/>
-      <h1 className='text-[4rem] bg-[#E2DDDD] h-[80px] font-extrabold'>Flowbite</h1>
+      <h1 className='text-[4rem] bg-[#E2DDDD] h-[80px] font-extrabold'>React Bootstrap</h1>
       <ReactBootstrap/>
     </>
   );
